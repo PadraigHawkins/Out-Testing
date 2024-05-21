6a46359a33195c48e180d4f2fe851556fb4a60dd
PadraigHawkins <107004777+PadraigHawkins@users.noreply.github.com>
2024-05-21 20:31:46 +0100

    commit

--- myscript.js
+++ myscript.js
@@ -10,13 +10,16 @@ const names = [
   { name: 'WHELANS', link: 'https://goo.gl/maps/ZDjDLsX1xnLqFk3P6' },
   { name: 'TAPPED', link: 'https://goo.gl/maps/XGagPobWsUWXoZH6A' },
   { name: '4 DAME LANE', link: 'https://goo.gl/maps/mvN6jMD5rXSffhsQ8' },
+  { name: 'GINGERMAN', link: 'https://maps.app.goo.gl/XiiKGxLNv9xpo6bL9' },
   { name: 'THE WOOL SHED', link: 'https://goo.gl/maps/t3sov5XTJtZMS4jE6' },
   { name: 'SIN E', link: 'https://goo.gl/maps/vvcHKfdNimswsR5t8' },
   { name: 'FLANNERYS', link: 'https://goo.gl/maps/rnCNfPHpLcpcV6o36' },
   { name: 'STAGS HEAD', link: 'https://goo.gl/maps/AkZgizjsmxcsQXjW9' },
   { name: 'KEHOES PUB', link: 'https://goo.gl/maps/Gqq3awQDAhDeDsqd7' },
+  { name: 'WALSHS', link: 'https://maps.app.goo.gl/fHdvZzTmfHK54xzS9' },
   { name: 'HOGANS', link: 'https://goo.gl/maps/yDVMHgVoGpisuK5e6' },
   { name: 'DOYLES', link: 'https://goo.gl/maps/hT3ntGK9gqxZAWzp8' },
+  { name: 'ONEILLS', link: 'https://maps.app.goo.gl/2HiN5R5vCyArQ1Mi7' },
   { name: 'SINNOTS BAR', link: 'https://goo.gl/maps/Yz2JL8iMvoUre2Qx9' },
   { name: 'THE CELT', link: 'https://goo.gl/maps/gJsQou8HNRm5uYUA6' },
   { name: 'SILVER PENNY', link: 'https://goo.gl/maps/oDqHqqpH5qxhXEecA' },
@@ -71,6 +74,8 @@ function selectGalway() {
 }
 
 
+
+
 // Dropdown
 
 function toggleDropdown() {
@@ -89,3 +94,8 @@ window.onclick = function(event) {
     }
   }
 }
+
+
+$('.btn').bind('click', function(){
+  $(this).toggleClass('active');
+});
